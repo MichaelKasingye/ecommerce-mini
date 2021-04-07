764eb0c1cfa42375e01d347a2d6ecc39173107b2
MichaelKasingye <michaelkasingye@gmail.com>
2021-04-07 18:10:10 +0300

    add meta

--- components/Meta.js
+++ components/Meta.js
@@ -9,6 +9,35 @@ const Meta = ({ title, keywords, description }) => {
       <meta charSet="utf-8" />
       <link rel="icon" href="/favicon.ico" />
 
+
+      <meta property="og:title" content="Shop site " />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://miniecommerce.netlify.app/" />
+    <meta
+      property="og:image"
+      content="https://i.pinimg.com/originals/7f/84/e6/7f84e6245c28a0e056b88c5c1b389cde.jpg"
+    />
+    <meta
+      property="og:description"
+      content="Mini e-shop that displays stock to order"
+    />
+
+    <meta property="twitter:card" content="Mini e-shop" />
+    <meta
+      property="twitter:url"
+      content="https://miniecommerce.netlify.app/"
+    />
+    <meta property="twitter:title" content="Mini e-shop" />
+    <meta
+      property="twitter:description"
+      content="Mini e-shop that displays stock to order."
+    />
+    <meta
+      property="twitter:image"
+      content="https://i.pinimg.com/originals/7f/84/e6/7f84e6245c28a0e056b88c5c1b389cde.jpg"
+    />
+
+
       <script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
@@ -21,9 +50,9 @@ const Meta = ({ title, keywords, description }) => {
 };
 
 Meta.defaultProps = {
-  title: "WebDev Newz",
-  keywords: "web development, programming",
-  description: "Get the latest news in web dev",
+  title: "Mini e-shop",
+  keywords: "e-commerce",
+  description: "Mini e-shop that displays stock to order",
 };
 
 export default Meta;
