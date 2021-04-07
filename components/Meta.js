1f0f4950a13034858d6cc3b9d6693c9f7cab6039
MichaelKasingye <michaelkasingye@gmail.com>
2021-04-07 17:47:07 +0300

    first commit

---
+++ components/Meta.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+
+const Meta = ({ title, keywords, description }) => {
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="keywords" content={keywords} />
+      <meta name="description" content={description} />
+      <meta charSet="utf-8" />
+      <link rel="icon" href="/favicon.ico" />
+
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
+        crossOrigin="anonymous"
+      ></script>
+
+      <title>{title}</title>
+    </Head>
+  );
+};
+
+Meta.defaultProps = {
+  title: "WebDev Newz",
+  keywords: "web development, programming",
+  description: "Get the latest news in web dev",
+};
+
+export default Meta;
